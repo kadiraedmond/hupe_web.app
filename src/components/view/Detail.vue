727eb03ede81863177ecf277391b3fcaa7152285
kadiraedmond <dimilinekadira505@gmail.com>
2023-10-07 10:32:12 +0000

    tttt

--- src/components/view/Detail.vue
+++ src/components/view/Detail.vue
@@ -4750,16 +4750,61 @@ import Footer from '@/components/Footer.vue';
                   <div class="col-md-12">
                     <div class="card h-100" id="card_compagnie">
                       <div class="card-body">
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                          laboris nisi ut aliquip ex ea commodo consequat Lorem ipsum dolor sit amet, consectetur
-                          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                          consequatLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                          ullamco laboris nisi ut aliquip ex ea commodo consequat
-                        </p>
+                          <div class="row">
+                            <div class="col-md-12">
+                              <p><strong>Raison sociale |</strong>   GB Compagnie</p>
+
+                              <p>  <strong> Responsable |</strong>   Mr joe</p>
+                            </div>
+
+                            <div class="row row-cols-1 row-cols-md-4 mb-4 g-4" style="margin-top: -5px;">
+                              <div class="col">
+                                <div class="card h-100 text-center border-0">
+                                  <div class="row mt-4" style="justify-content: center;">
+                                    <img src="assets/img/icone/mail.png" class="img-fluid" alt="..." style="width: 64px">
+                                  </div>
+                                  
+                                  <div class="card-body">
+                                    <h5 class="card-title" style="font-size: 14px">compagniegb@gmail.com</h5>
+                                   </div>
+                                </div>
+                              </div>
+                              <div class="col">
+                                <div class="card h-100 text-center border-0">
+                                  <div class="row mt-4" style="justify-content: center;">
+                                    <img src="assets/img/icone/web.png" class="img-fluid" alt="..." style="width: 64px">
+                                  </div>
+                                  
+                                  <div class="card-body">
+                                    <h5 class="card-title" style="font-size: 14px">compagniegb.com</h5>
+                                    <h5 class="card-title" style="font-size: 14px">+000 000 000 00</h5>
+                                   </div>
+                                </div>
+                              </div>
+                              <div class="col">
+                                <div class="card h-100 text-center border-0">
+                                  <div class="row mt-4" style="justify-content: center;">
+                                    <img src="assets/img/icone/maps.png" class="img-fluid" alt="..." style="width: 64px">
+                                  </div>
+                                  
+                                  <div class="card-body">
+                                    <h5 class="card-title" style="font-size: 14px">Jean Dupont,10 Rue des Palmiers ,Quartier des Cocotiers</h5>
+                                   </div>
+                                </div>
+                              </div>
+                              <div class="col">
+                                <div class="card h-100 text-center border-0">
+                                  <div class="row mt-4" style="justify-content: center;">
+                                    <img src="assets/img/icone/location.png" class="img-fluid" alt="..." style="width: 64px">
+                                  </div>
+                                  
+                                  <div class="card-body">
+                                    <h5 class="card-title" style="font-size: 14px">1223.2522.55552</h5>
+                                   </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
 
                         <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
                       </div>
