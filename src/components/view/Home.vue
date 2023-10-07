14ee7a6c38b377fadbf565c2f23e1b45ad27fc98
kadiraedmond <dimilinekadira505@gmail.com>
2023-10-07 18:43:39 +0000

    home

--- src/components/view/Home.vue
+++ src/components/view/Home.vue
@@ -132,7 +132,7 @@ import Navbar from '@/components/Navbar.vue';
           <div class="col">
             <div class="card h-100 border-0 text-center" style="background:#f6f8fb;">
               <a v-bind:href="'/location'" class="mt-4">
-                  <img src="./public/assets/img/car.png" class="img-fluid w-25" alt="...">
+                  <img src="/public/assets/img/car.png" class="img-fluid w-25" alt="...">
               </a>
               
               <div class="card-body" style=" background: #219935;">
@@ -147,7 +147,7 @@ import Navbar from '@/components/Navbar.vue';
           <div class="col">
               <div class="card h-100 border-0 text-center" style="background:#f6f8fb;">
                 <a v-bind:href="'/reservation'" class="mt-4 text-black">
-                    <img src="./public/assets/img/bus.png" class="img-fluid w-25" alt="...">
+                    <img src="/public/assets/img/bus.png" class="img-fluid w-25" alt="...">
                 </a>
                 
                 <div class="card-body" style=" background: #219935;">
@@ -162,7 +162,7 @@ import Navbar from '@/components/Navbar.vue';
           <div class="col">
               <div class="card h-100 border-0 text-center" style="background:#f6f8fb;">
                 <a v-bind:href="'/reservation'" class="mt-4" id="a">
-                    <img src="./public/assets/img/engin.png" class="img-fluid w-25" alt="...">
+                    <img src="/public/assets/img/engin.png" class="img-fluid w-25" alt="...">
                 </a>
                 
                 <div class="card-body" style=" background: #219935;">
@@ -177,7 +177,7 @@ import Navbar from '@/components/Navbar.vue';
           <div class="col">
               <div class="card h-100 border-0 text-center" style="background:#f6f8fb;">
                 <a v-bind:href="'/reservation'" class="mt-4" id="a">
-                    <img src="./public/assets/img/engine.png" class="img-fluid w-25" alt="...">
+                    <img src="/public/assets/img/engine.png" class="img-fluid w-25" alt="...">
                 </a>
                 
                 <div class="card-body" style=" background: #219935;">
@@ -215,15 +215,15 @@ import Navbar from '@/components/Navbar.vue';
             <div class="card" style="background:#a6a6a621;">
                <div class="row" style="padding: 6px;"> 
                 <div class="col-md-12 d-flex"> 
-                    <img src="./public/assets/img/icone/car.png" class="img-fluid" alt="..." style=" width: 25px; height: 25px; margin-top: 6px;" >
+                    <img src="/public/assets/img/icone/car.png" class="img-fluid" alt="..." style=" width: 25px; height: 25px; margin-top: 6px;" >
                   <h6 style="font-size: 12px; margin-left: 5px; margin-top: 10px;"> Compagagnie test</h6>
-                  <p style="font-size: 12px;  margin-left: 5px;  margin-top: 6px;"><img src="./public/assets/img/icone/map.png" class="img-fluid" alt="..." > logone</p>
+                  <p style="font-size: 12px;  margin-left: 5px;  margin-top: 6px;"><img src="/public/assets/img/icone/map.png" class="img-fluid" alt="..." > logone</p>
                   
                 </div>
                </div>
               <div class="card h-100" id="compagnie_card" style="padding: 6px; background:#a6a6a621;">
                 <a v-bind:href="'/detail'" style="border: 1px solid; border-radius: 5px;  border-color: #a6a6a6;">
-                  <img src="./public/assets/img/car3.jpg" class="card-img-top" alt="..." style="border-radius: 5px 5px 5px 5px; height: 215px !important; object-fit: cover;">
+                  <img src="/public/assets/img/car3.jpg" class="card-img-top" alt="..." style="border-radius: 5px 5px 5px 5px; height: 215px !important; object-fit: cover;">
                 </a>
                 <button class="btn btn-primary" id="badges"> <s> 5000 FCFA </s></button>
                 <button class="btn btn-primary" id="badges0"> 2000 FCFA</button>
@@ -236,15 +236,15 @@ import Navbar from '@/components/Navbar.vue';
             <div class="card" style="background:#a6a6a621;">
                <div class="row" style="padding: 6px;"> 
                 <div class="col-md-12 d-flex"> 
-                    <img src="./public/assets/img/icone/car.png" class="img-fluid" alt="..." style=" width: 25px; height: 25px; margin-top: 6px;" >
+                    <img src="/public/assets/img/icone/car.png" class="img-fluid" alt="..." style=" width: 25px; height: 25px; margin-top: 6px;" >
                   <h6 style="font-size: 12px; margin-left: 5px; margin-top: 10px;"> Compagagnie test</h6>
-                  <p style="font-size: 12px;  margin-left: 5px;  margin-top: 6px;"><img src="./public/assets/img/icone/map.png" class="img-fluid" alt="..." > logone</p>
+                  <p style="font-size: 12px;  margin-left: 5px;  margin-top: 6px;"><img src="/public/assets/img/icone/map.png" class="img-fluid" alt="..." > logone</p>
                   
                 </div>
                </div>
               <div class="card h-100" id="compagnie_card" style="padding: 6px; background:#a6a6a621;">
                 <a v-bind:href="'/detail'" style="border: 1px solid; border-radius: 5px;  border-color: #a6a6a6;">
-                  <img src="./public/assets/img/car3.jpg" class="card-img-top" alt="..." style="border-radius: 5px 5px 5px 5px; height: 215px !important; object-fit: cover;">
+                  <img src="/public/assets/img/car3.jpg" class="card-img-top" alt="..." style="border-radius: 5px 5px 5px 5px; height: 215px !important; object-fit: cover;">
                 </a>
                 <button class="btn btn-primary" id="badges"> <s> 5000 FCFA </s></button>
                 <button class="btn btn-primary" id="badges0"> 2000 FCFA</button>
@@ -257,15 +257,15 @@ import Navbar from '@/components/Navbar.vue';
             <div class="card" style="background:#a6a6a621;">
                <div class="row" style="padding: 6px;"> 
                 <div class="col-md-12 d-flex"> 
-                    <img src="./public/assets/img/icone/car.png" class="img-fluid" alt="..." style=" width: 25px; height: 25px; margin-top: 6px;" >
+                    <img src="/public/assets/img/icone/car.png" class="img-fluid" alt="..." style=" width: 25px; height: 25px; margin-top: 6px;" >
                   <h6 style="font-size: 12px; margin-left: 5px; margin-top: 10px;"> Compagagnie test</h6>
-                  <p style="font-size: 12px;  margin-left: 5px;  margin-top: 6px;"><img src="./public/assets/img/icone/map.png" class="img-fluid" alt="..." > logone</p>
+                  <p style="font-size: 12px;  margin-left: 5px;  margin-top: 6px;"><img src="/public/assets/img/icone/map.png" class="img-fluid" alt="..." > logone</p>
                   
                 </div>
                </div>
               <div class="card h-100" id="compagnie_card" style="padding: 6px; background:#a6a6a621;">
                 <a v-bind:href="'/detail'" style="border: 1px solid; border-radius: 5px;  border-color: #a6a6a6;">
-                  <img src="./public/assets/img/car3.jpg" class="card-img-top" alt="..." style="border-radius: 5px 5px 5px 5px; height: 215px !important; object-fit: cover;">
+                  <img src="/public/assets/img/car3.jpg" class="card-img-top" alt="..." style="border-radius: 5px 5px 5px 5px; height: 215px !important; object-fit: cover;">
                 </a>
                 <button class="btn btn-primary" id="badges"> <s> 5000 FCFA </s></button>
                 <button class="btn btn-primary" id="badges0"> 2000 FCFA</button>
@@ -302,7 +302,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
               <a v-bind:href="'/detail'">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
               </a>
 
               <div class="card-body">
@@ -330,7 +330,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
               <a v-bind:href="'/detail'">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
               </a>
                  
               
@@ -361,7 +361,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
               <a href="detail_compagnie.html">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
               </a>
 
               <div class="card-body">
@@ -386,7 +386,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
               <a href="detail_compagnie.html">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
               </a>
 
               <div class="card-body">
@@ -436,7 +436,7 @@ import Navbar from '@/components/Navbar.vue';
                   <div class="card mb-3 border-0" style="max-width: 540px;">
                     <div class="row g-1">
                       <div class="col-md-4">
-                        <img src="./public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
+                        <img src="/public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
                       </div>
                       <div class="col-md-8">
                         <div class="card-body">
@@ -457,7 +457,7 @@ import Navbar from '@/components/Navbar.vue';
               <div class="card mb-3 mt-4" style="max-width: 540px; margin: 10px; margin-top: -10px !important;">
                 <div class="row g-0" style="margin: 10px;">
                   <div class="col-md-4">
-                    <img src="./public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
+                    <img src="/public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
@@ -479,7 +479,7 @@ import Navbar from '@/components/Navbar.vue';
                   <div class="card mb-3 border-0" style="max-width: 540px;">
                     <div class="row g-1">
                       <div class="col-md-4">
-                        <img src="./public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
+                        <img src="/public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
                       </div>
                       <div class="col-md-8">
                         <div class="card-body">
@@ -500,7 +500,7 @@ import Navbar from '@/components/Navbar.vue';
               <div class="card mb-3 mt-4" style="max-width: 540px; margin: 10px; margin-top: -10px !important;">
                 <div class="row g-0" style="margin: 10px;">
                   <div class="col-md-4">
-                    <img src="./public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
+                    <img src="/public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
@@ -542,7 +542,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
                 <router-link to="/details">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
                </router-link>
 
               <div class="card-body">
@@ -567,7 +567,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
                 <router-link to="/details">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
                </router-link>
 
               <div class="card-body">
@@ -592,7 +592,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
                 <router-link to="/details">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
                </router-link>
 
               <div class="card-body">
@@ -617,7 +617,7 @@ import Navbar from '@/components/Navbar.vue';
 
             <div class="card h-100" id=compagnie_card>
               <a href="detail_compagnies.html">
-                <img src="./public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
+                <img src="/public/assets/img/car2.jpg" class="card-img-top" alt="..." style="border-radius: 10px 10px 0px 0px;">
               </a>
 
               <div class="card-body">
@@ -667,7 +667,7 @@ import Navbar from '@/components/Navbar.vue';
                   <div class="card mb-3 border-0" style="max-width: 540px;">
                     <div class="row g-1">
                       <div class="col-md-4">
-                        <img src="./public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
+                        <img src="/public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
                       </div>
                       <div class="col-md-8">
                         <div class="card-body">
@@ -688,7 +688,7 @@ import Navbar from '@/components/Navbar.vue';
               <div class="card mb-3 mt-4" style="max-width: 540px; margin: 10px; margin-top: -10px !important;">
                 <div class="row g-0" style="margin: 10px;">
                   <div class="col-md-4">
-                    <img src="./public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
+                    <img src="/public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
@@ -710,7 +710,7 @@ import Navbar from '@/components/Navbar.vue';
                   <div class="card mb-3 border-0" style="max-width: 540px;">
                     <div class="row g-1">
                       <div class="col-md-4">
-                        <img src="./public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
+                        <img src="/public/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle"  style="width: 90px;"/>
                       </div>
                       <div class="col-md-8">
                         <div class="card-body">
@@ -731,7 +731,7 @@ import Navbar from '@/components/Navbar.vue';
               <div class="card mb-3 mt-4" style="max-width: 540px; margin: 10px; margin-top: -10px !important;">
                 <div class="row g-0" style="margin: 10px;">
                   <div class="col-md-4">
-                    <img src="./public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
+                    <img src="/public/assets/img/car2.jpg" class="img-fluid rounded-start h-100" alt="...">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
@@ -770,7 +770,7 @@ import Navbar from '@/components/Navbar.vue';
         <div class="row row-cols-1 row-cols-md-4 g-4">
           <div class="col">
             <div class="card h-100 border-0" style="box-shadow: 5px 5px 10px rgba(0,0,0,.2);">
-              <img src="./public/assets/img/blog.jpg" class="card-img-top" alt="...">
+              <img src="/public/assets/img/blog.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <div class="row" id="blog_row">
                   <div class="col-md-12">
@@ -797,7 +797,7 @@ import Navbar from '@/components/Navbar.vue';
           </div>
           <div class="col">
             <div class="card h-100 border-0" style="box-shadow: 5px 5px 10px rgba(0,0,0,.2);">
-              <img src="./public/assets/img/blog-1.jpg" class="card-img-top" alt="...">
+              <img src="/public/assets/img/blog-1.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <div class="row" id="blog_row">
                   <div class="col-md-12">
@@ -824,7 +824,7 @@ import Navbar from '@/components/Navbar.vue';
           </div>
           <div class="col">
             <div class="card h-100 border-0" style="box-shadow: 5px 5px 10px rgba(0,0,0,.2);">
-              <img src="./public/assets/img/blog-2.jpg" class="card-img-top" alt="...">
+              <img src="/public/assets/img/blog-2.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <div class="row" id="blog_row">
                   <div class="col-md-12">
@@ -851,7 +851,7 @@ import Navbar from '@/components/Navbar.vue';
           </div>
           <div class="col">
             <div class="card h-100 border-0" style="box-shadow: 5px 5px 10px rgba(0,0,0,.2);">
-              <img src="./public/assets/img/blog-2.jpg" class="card-img-top" alt="...">
+              <img src="/public/assets/img/blog-2.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <div class="row" id="blog_row">
                   <div class="col-md-12">
