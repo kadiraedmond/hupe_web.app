b836079c4a03341a9e1943a1d39333e68b754683
kadiraedmond <dimilinekadira505@gmail.com>
2024-03-28 01:16:13 +0000

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -206,6 +206,20 @@ const handleSelect = (selectedCountry) => {
   document.querySelector('#closeSelectCountryModal').click()
 };
 
+ 
+
+const isMobileNavOpen = ref(false);
+
+function toggleMobileNav() {
+  isMobileNavOpen.value = !isMobileNavOpen.value;
+}
+
+function handleRouterLinkClick() {
+  closeMobileNav();
+}
+
+router.afterEach(handleRouterLinkClick);
+
 </script>
 
 <template>
@@ -301,20 +315,7 @@ const handleSelect = (selectedCountry) => {
       <!-- <button class="btn btn-primary" type="submit">Rechercher</button> -->
     </form>
 
-     
-      <!-- <form class="d-flex" role="search" @submit.prevent="handleSearch">
-        <input
-          class="form-control me-2"
-          style="width: 200; max-width: 200px; font-size: 13px;"
-          type="search"
-          placeholder="Recherche"
-          v-model="searchTerm"
-          aria-label="Search"
-          id="search"
-        />
-        <i class="bx bx-search" type="submit" id="icon_search"></i>
-        
-      </form> -->
+      
 
       <nav id="navbar" class="navbar">
         <ul>
@@ -414,8 +415,112 @@ const handleSelect = (selectedCountry) => {
           </li>
 
         </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+
+         
+        <!-- <i class="bi bi-list mobile-nav-toggle"></i> -->
+        <i class="bi" :class="['mobile-nav-toggle', isMobileNavOpen ? 'bi-x' : 'bi-list']" @click="toggleMobileNav"></i>
+       
+        <nav id="navbarm" :class="{ 'navbar navbar-mobile': isMobileNavOpen }">
+          <ul>
+            <li>
+              <router-link @click="home" to="" class="nav-link scrollto " :class="{ active: $route.path === '/' }"
+                ><i class="bx bx-home" id="icon_menu"></i> {{ t('home') }}</router-link
+              >
+            </li>
+            
+            <li>
+              <router-link to="/services" class="nav-link scrollto" :class="{ active: $route.path === '/services' }"
+                ><i class="bx bx-category" id="icon_menu"></i>
+                Services</router-link
+              >
+            </li>
+            <li>
+              <router-link class="nav-link scrollto" to="/service_client" :class="{ active: $route.path === '/service_client' }"
+                ><i class="bx bx-help-circle" id="icon_menu"></i> {{ t('help') }}
+              </router-link>
+            </li>
+            <li>
+              <router-link class="nav-link scrollto" to="/promotion" :class="{ active: $route.path === '/promotion' }"
+                ><i class="bx bxs-megaphone" id="icon_menu"></i> Promotions
+              </router-link>
+            </li>
+            <li style="margin-left: 30px; font-size: 14px; font-weight: 600">
+              <select v-model="selectedLanguage" @change="changeLanguage" class="form-select1 px-3" style="padding: 8px" id="validationCustom04" required>
+                <option selected value="fr">Francais</option>
+                <option value="en">Anglais</option>
+              </select>
+            </li>
+
+            <li>
+              <router-link v-if="!authStore.user.uid && !savedUser" to="/type-utilisateur" class="nav-link scrollto" :class="{ active: $route.path === '/connexion' }"
+                ><i class="bx bx-user" id="icon_menu"></i> Connexion
+              </router-link>
+            </li>
+            <li class="dropdown">
+              <router-link to=""
+                v-if="authStore.user.uid || savedUser"
+                ><i class="bx bx-user" id="icon_menu"></i>
+                <span>
+                  {{ 
+                    savedUser.raison_social !== '' ? savedUser.raison_social : savedUser.username !== '' && savedUser.username
+                    || authStore.user.raison_social !== '' ? authStore.user.raison_social : authStore.user.username !== '' && authStore.user.username
+                  }} 
+                </span> <i class="bi bi-chevron-down"></i
+              ></router-link>
+              <ul style="background: #219935; color: white">
+                <li @click="goTo_location" style="color: white">
+                  <router-link 
+                    to="" 
+                    style="color: white"
+                    v-if="(savedUser && savedUser.type_compagnie == 'Location') 
+                          || (user.raison_social && user.type_compagnie == 'Location') 
+                          || (authStore.isConnected && authStore.isLocationCompany)"
+                    >Compte location de véhicules</router-link
+                  >
+                </li>
+                <li @click="goTo_reservation" style="color: white">
+                  <router-link to="" 
+                  style="color: white"
+                  v-if="(savedUser && savedUser.type_compagnie == 'Transport') 
+                        || (user.raison_social && user.type_compagnie == 'Transport') 
+                        || (authStore.isConnected && authStore.isReservationCompany)"
+                    >Compte réservation de tickets de bus</router-link
+                  >
+                </li>
+                <li style="color: white">
+                  <router-link style="color: white" v-if="authStore.isConnected && authStore.isBigEnginsCompany || user.raison_social" to="/compte_gros_engin"
+                    >Compte location de gros engins</router-link
+                  >
+                </li>
+                <li style="color: white">
+                  <router-link style="color: white" v-if="authStore.isConnected && authStore.isCarsSellingCompany || user.raison_social" to="/compte_achat_engin">Compte vente d'engins </router-link>
+                </li>
+                <li @click="goTo_client" style="color: white">
+                  <router-link 
+                    style="color: white"
+                    v-if="(authStore.user.uid && !authStore.user.raison_social) 
+                          || (savedUser && !savedUser.raison_social)" 
+                    to="">
+                    {{ t('my_account') }}
+                  </router-link>
+                </li>
+                <li style="color: white"><router-link style="color: white" v-if="authStore.user.uid || savedUser" to="/" @click="logout">{{ t('logout') }}</router-link></li>
+                
+              </ul>
+            </li>
+            <li>
+              <router-link v-if="authStore.user.uid || savedUser" class="nav-link scrollto" :to="`/notification`" :class="{ active: $route.path === '/notification' }"
+              ><i class="bx bxs-bell" id="icon_menu"></i> 
+                <p style="position: absolute; right: -8px; top: -10px; color: white; background: #E00; border-radius: 100%; padding: 0 2px">
+                  {{ notificationStore.count > 0 ? `+${notificationStore.count}` : '' }} 
+                </p>
+              </router-link>
+            </li>
+
+          </ul>
       </nav>
+      </nav>
+      
       <!-- .navbar -->
     </div>
   </header>
@@ -477,6 +582,30 @@ const handleSelect = (selectedCountry) => {
   -moz-appearance: none;
   appearance: none;
   border-color: #219935;
+}
+
+.navbar-mobile .form-select1 {
+    display: block;
+    width: 34%;
+    padding: .375rem 2.25rem .375rem .75rem;
+    -moz-padding-start: calc(0.75rem - 3px);
+    font-size: 14px;
+    font-weight: 600;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    line-height: 1.5;
+    color: #219935;
+    background-color: white;
+    background-repeat: no-repeat;
+    background-position: right .75rem center;
+    background-size: 16px 12px;
+    border: 1px solid #219935;
+    border-radius: .375rem;
+    /* transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out; */
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-color: #219935;
 }
 
 input::placeholder {
